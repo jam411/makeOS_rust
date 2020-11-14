e72133d2bfb91877defa5710ec3958dddc7fd100
James <r.j.shimizu@jaist.ac.jp>
2020-11-14 18:13:00 +0900

    fix the main.rs error

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,13 +1,13 @@
 // main.rs
 
+#![no_std]
+
+fn main() {}
+
 use core::panic::PanicInfo;
 
 // This function is called on panic.
 #[panic_handler]
 fn painc(_info: &PanicInfo) -> ! {
     loop {}
-}
-
-fn main() {
-
 }
