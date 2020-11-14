dd74e146df56f1ac4951a57a14dead86c54b4a4e
James <r.j.shimizu@jaist.ac.jp>
2020-11-14 18:03:31 +0900

    add new file2

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,6 +1,13 @@
 // main.rs
 
-#![no_std]
+use core::panic::PanicInfo;
+
+// This function is called on panic.
+#[panic_handler]
+fn painc(_info: &PanicInfo) -> ! {
+    loop {}
+}
 
 fn main() {
+
 }
