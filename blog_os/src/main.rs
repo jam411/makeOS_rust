460294aba30272a8a307f0f1c3a5dd9cf6eb8716
James <r.j.shimizu@jaist.ac.jp>
2020-11-14 18:20:58 +0900

    ugly linker error!!

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,13 +1,17 @@
 // main.rs
 
 #![no_std]
-
-fn main() {}
+#![no_main]
 
 use core::panic::PanicInfo;
 
 // This function is called on panic.
 #[panic_handler]
 fn painc(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
